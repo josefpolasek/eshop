f378f9811d623d930a0969b4d8f44aa39e0654d1
tomaspycha <tompycha11@gmail.com>
2021-12-03 12:12:32 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,12 +1,10 @@
 
 console.log("ahoj");
-//$("#window_signin").hide();
 var loginInputMail = document.getElementById("login_input_mail");
 
 $("#login_input_mail").click(function(){
 
   $("#login_input_mail").css("padding-bottom","5px");
-
 
 });
 
@@ -22,35 +20,134 @@ $("#register_to_login_btn").click(function(){
   $("#window_signin").animate({right: '0%',opacity: '0'});
 });
 
+// on default, there are no errors
+var error_lname = false;
+var error_fname = false;
+var error_gender = false;
+var error_mail = false;
+var error_password = false;
+var error_password_confirm = false;
 
-/*
+var error_mail_log = false;
+var error_password_log = false;
+
+//adding reactions to interactions with input fields
+$("#signup_input_fname").keyup(function(){
+  console.log("ahoj");
+  FNameCheck();
+});
+$("#signup_input_lname").keyup(function(){
+  LNameCheck();
+});
+$("#signup_input_mail").keyup(function(){
+  MailCheck();
+});
+$("#signup_input_pass").keyup(function(){
+  PasswordCheck();
+});
+$("#signup_input_confpass").keyup(function(){
+  PasswordConfirmCheck();
+});
+$("#login_input_mail").keyup(function(){
+  console.log("ahoj");
+  MailLogCheck();
+});
+$("#login_input_pass").keyup(function(){
+  PasswordLogCheck();
+});
+
+//click Submit button
+$("#btn_submit").click(function(){
+  FNameCheck();
+  LNameCheck();
+  MailCheck();
+  GenderCheck();
+  PasswordCheck();
+  PasswordConfirmCheck();
+  //is every value correct?
+  if (error_fname===true || error_lname===true || error_mail===true || error_gender===true || error_password===true || error_message_password_confirm===true) {
+
+    console.log("Problem!");
+  } else {
+
+    var fnameVar = $("#fname").val();
+    var lnameVar = $("#lname").val();
+    var genderVar = $("#gender").val();
+    var mailVar = $("#mail").val();
+    var passwordVar = $("#password").val();
+
+    //saving the credentials to local storage
+    localStorage.setItem('fname', fnameVar);
+    localStorage.setItem('lname', lnameVar);
+    localStorage.setItem('gender', genderVar);
+    localStorage.setItem('mail', mailVar);
+    localStorage.setItem('password', passwordVar);
+
+    console.log("OK");
+
+    //change the shown divs
+    $("#sign_up_box").hide();
+    $("#signed_box").show();
+
+
+  }
+
+});
+
+$("#btn_submit_log").click(function(){
+
+  MailLogCheck();
+  PasswordLogCheck();
+
+  //is every value correct?
+  if (error_mail_log===true || error_password_log===true) {
+
+    console.log("problem");
+  } else {
+
+    if (localStorage.getItem('mail')===$("#mail_log").val() && localStorage.getItem('password')===$("#password_log").val()) {
+      console.log("Credentials OK");
+      $("#login_box").hide();
+      $(".top").hide();
+      $("#logged_box").show();
+
+
+    } else {
+
+      $("#info").html("Credentials do not match!");
+      $("#info").css("color", "red");
+    }
+  }
+
+});
+
 
 //check the validity of First name
 function FNameCheck() {
 
   var pattern = /^[A-Z][a-z]*$/;
-  var fname = $("#fname").val();
+  var fname = $("#signup_input_fname").val();
   if (fname === '') {
-    $("#error_message_fname").html("Please enter your first name!");
-    $("#error_message_fname").show();
-    $(".inside_box#fname_box").css("border-color","red");
-    $("#tick_fname").hide();
+    //$("#error_message_fname").html("Please enter your first name!");
+    //$("#error_message_fname").show();
+
+    $("#signup_input_fname").css("border-color","red");
+    $("#signup_input_fname").css("padding-bottom","0px");
+    console.log("succes");
     error_fname = true;
   } else if (!pattern.test(fname)) {
 
-    $("#error_message_fname").html("First name cannot contain numbers!");
-    $("#error_message_fname").show();
-    $(".inside_box#fname_box").css("border-color","red");
-    $("#tick_fname").hide();
+    //$("#error_message_fname").html("First name cannot contain numbers!");
+    //$("#error_message_fname").show();
+    $("#signup_input_fname").css("border-color","red");
     error_fname = true;
   } else {
-    $("#error_message_fname").hide();
-    $(".inside_box#fname_box").css("border-color","white");
-    $("#tick_fname").show();
+    //$("#error_message_fname").hide();
+    $("#signup_input_fname").css("border-color","green");
     error_fname = false;
   }
 }
-
+/*
 //check the validity of Last name
 function LNameCheck() {
 
