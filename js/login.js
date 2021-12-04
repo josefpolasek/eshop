6db1808f57eedadf5f39fc9867fed16e54af3b3e
tomaspycha <tompycha11@gmail.com>
2021-12-04 15:23:33 +0900

    Dokonceni hrube casti loginpage

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
 
 console.log("ahoj");
+
+$(".error").hide();
 var loginInputMail = document.getElementById("login_input_mail");
 
 $("#login_input_mail").click(function(){
 
-  $("#login_input_mail").css("padding-bottom","5px");
+  //$("#login_input_mail").css("padding-bottom","5px");
 
 });
 
@@ -128,8 +130,8 @@ function FNameCheck() {
   var pattern = /^[A-Z][a-z]*$/;
   var fname = $("#signup_input_fname").val();
   if (fname === '') {
-    //$("#error_message_fname").html("Please enter your first name!");
-    //$("#error_message_fname").show();
+    $("#error_message_fname").html("Please enter your first name!");
+    $("#error_message_fname").show();
 
     $("#signup_input_fname").css("border-color","red");
     $("#signup_input_fname").css("padding-bottom","0px");
@@ -137,81 +139,63 @@ function FNameCheck() {
     error_fname = true;
   } else if (!pattern.test(fname)) {
 
-    //$("#error_message_fname").html("First name cannot contain numbers!");
-    //$("#error_message_fname").show();
+    $("#error_message_fname").html("First name cannot contain numbers!");
+    $("#error_message_fname").show();
     $("#signup_input_fname").css("border-color","red");
     error_fname = true;
   } else {
-    //$("#error_message_fname").hide();
+    $("#error_message_fname").hide();
     $("#signup_input_fname").css("border-color","green");
     error_fname = false;
   }
 }
-/*
+
 //check the validity of Last name
 function LNameCheck() {
 
   var pattern = /^[A-Z][a-z]*$/;
-  var lname = $("#lname").val();
+  var lname = $("#signup_input_lname").val();
   if (lname === '') {
     $("#error_message_lname").html("Please enter your last name!");
     $("#error_message_lname").show();
-    $(".inside_box#lname_box").css("border-color","red");
-    $("#tick_lname").hide();
+    $("#signup_input_lname").css("border-color","red");
+    $("#signup_input_lname").css("padding-bottom","0px");
     error_lname = true;
   } else if (!pattern.test(lname)) {
 
     $("#error_message_lname").html("Last name cannot contain numbers!");
     $("#error_message_lname").show();
-    $(".inside_box#lname_box").css("border-color","red");
-    $("#tick_lname").hide();
+    $("#signup_input_lname").css("border-color","red");
+    $("#signup_input_lname").css("padding-bottom","0px");
     error_lname = true;
   } else {
     $("#error_message_lname").hide();
-    $(".inside_box#lname_box").css("border-color","white");
-    $("#tick_lname").show();
+    $("#signup_input_lname").css("border-color","green");
     error_lname = false;
   }
 }
 
-//check the validity of gender choice
-function GenderCheck() {
-
-  var male = document.getElementById("gender_male").checked;
-  var female = document.getElementById("gender_female").checked;
-  if (male==false && female==false) {
-    $("#error_message_gender").html("Please enter your gender!");
-    $("#error_message_gender").show();
-    $("#tick_gender").hide();
-    error_gender = true;
-    } else {
-    $("#error_message_gender").hide();
-    $("#tick_gender").show();
-    error_gender = false;
-  }
-}
 //check the validity of Email address
 function MailCheck() {
 
   var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-  var mail = $("#mail").val();
+  var mail = $("#signup_input_mail").val();
   if (mail === '') {
     $("#error_message_mail").html("Please enter your email!");
     $("#error_message_mail").show();
-    $(".inside_box#mail_box").css("border-color","red");
-    $("#tick_mail").hide();
+    $("#signup_input_mail").css("border-color","red");
+    $("#signup_input_mail").css("padding-bottom","0px");
     error_mail = true;
   } else if (!pattern.test(mail)) {
 
     $("#error_message_mail").html("Yours email address is invalid!");
     $("#error_message_mail").show();
-    $(".inside_box#mail_box").css("border-color","red");
-    $("#tick_mail").hide();
+    $("#signup_input_mail").css("border-color","red");
+    $("#signup_input_mail").css("padding-bottom","0px");
     error_mail = true;
   } else {
     $("#error_message_mail").hide();
-    $(".inside_box#mail_box").css("border-color","white");
-    $("#tick_mail").show();
+    $("#signup_input_mail").css("border-color","green");
     error_mail = false;
   }
 }
@@ -219,30 +203,25 @@ function MailCheck() {
 function PasswordCheck() {
 
   var pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-  var password = $("#password").val();
+  var password = $("#signup_input_pass").val();
   if (password === '') {
-    $("#error_message_password").html("Please enter your password!");
-    $("#error_message_password").css("height","20px");
-    $("#error_message_password").css("text-align","left");
+    $("#error_message_pass").html("Please enter your password!");
+    $("#error_message_pass").show();
 
-    $("#error_message_password").show();
-    $(".inside_box#password_box").css("border-color","red");
-    $("#tick_password").hide();
+    $("#signup_input_pass").css("border-color","red");
+    $("#signup_input_pass").css("padding-bottom","0px");
     error_password = true;
   } else if (!pattern.test(password)) {
 
-    $("#error_message_password").html("Requirement: at least 6 characters, one capital letter,<br> one lowercase letter, at least one digit and one special<br> chararcter!");
-    $("#error_message_password").css("height","40px");
-    $("#error_message_password").css("text-align","center");
+    $("#error_message_pass").html("Requirement: at least 6 characters, one capital letter,<br> one lowercase letter, at least one digit and one special<br> chararcter!");
+    $("#error_message_pass").show();
 
-    $("#error_message_password").show();
-    $(".inside_box#password_box").css("border-color","red");
-    $("#tick_password").hide();
+    $("#signup_input_pass").css("border-color","red");
+    $("#signup_input_pass").css("padding-bottom","0px");
     error_password = true;
   } else {
-    $("#error_message_password").hide();
-    $(".inside_box#password_box").css("border-color","white");
-    $("#tick_password").show();
+    $("#error_message_pass").hide();
+    $("#signup_input_pass").css("border-color","green");
     error_password = false;
     password_ref = password;
   }
@@ -250,27 +229,28 @@ function PasswordCheck() {
 //check the validity of password confirmation
 function PasswordConfirmCheck() {
 
-  var password = $("#password").val();
-  var password_confirm = $("#password_confirm").val();
+  var password = $("#signup_input_pass").val();
+  var password_confirm = $("#signup_input_confpass").val();
 
 
   if (password_confirm === "") {
-    $("#error_message_password_confirm").html("Please re-enter your password!");
-    $("#error_message_password_confirm").show();
-    $(".inside_box#password_confirm_box").css("border-color","red");
-    $("#tick_password_confirm").hide();
+    $("#error_message_passconf").html("Please re-enter your password!");
+    $("#error_message_passconf").show();
+
+    $("#signup_input_confpass").css("border-color","red");
+    $("#signup_input_confpass").css("padding-bottom","0px");
     error_password_confirm = true;
 
   } else if (password_ref === password_confirm) {
-     $("#error_message_password_confirm").hide();
-     $(".inside_box#password_confirm_box").css("border-color","white");
-     $("#tick_password_confirm").show();
+     $("#error_message_passconf").hide();
+     $("#signup_input_confpass").css("border-color","green");
+
      error_password_confirm = false;
   } else {
-     $("#error_message_password_confirm").html("Password does not match!");
-     $("#error_message_password_confirm").show();
-     $(".inside_box#password_confirm_box").css("border-color","red");
-     $("#tick_password_confirm").hide();
+     $("#error_message_passconf").html("Password does not match!");
+     $("#error_message_passconf").show();
+     $("#signup_input_confpass").css("border-color","red");
+     $("#signup_input_confpass").css("padding-bottom","0px");
      error_password_confirm = true;
   }
 }
@@ -279,24 +259,24 @@ function PasswordConfirmCheck() {
 function MailLogCheck() {
 
   var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-  var mail = $("#mail_log").val();
+  var mail = $("#login_input_mail").val();
   if (mail === '') {
     $("#error_message_mail_log").html("Please enter your email!");
     $("#error_message_mail_log").show();
-    $(".inside_box#mail_box_log").css("border-color","red");
-    $("#tick_mail_log").hide();
+    $("#login_input_mail").css("border-color","red");
+    //$("#login_input_mail").css("padding-bottom","0px");
     error_mail_log = true;
   } else if (!pattern.test(mail)) {
 
     $("#error_message_mail_log").html("Yours email address is invalid!");
     $("#error_message_mail_log").show();
-    $(".inside_box#mail_box_log").css("border-color","red");
-    $("#tick_mail_log").hide();
+    $("#login_input_mail").css("border-color","red");
+    //$("#login_input_mail").css("padding-bottom","0px");
+
     error_mail_log = true;
   } else {
     $("#error_message_mail_log").hide();
-    $(".inside_box#mail_box_log").css("border-color","white");
-    $("#tick_mail_log").show();
+    $("#login_input_mail").css("border-color","green");
     error_mail_log = false;
   }
 }
@@ -304,21 +284,16 @@ function MailLogCheck() {
 function PasswordLogCheck() {
 
   var pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-  var password = $("#password_log").val();
+  var password = $("#login_input_pass").val();
   if (password === '') {
-    $("#error_message_password_log").html("Please enter your password!");
-    $("#error_message_password_log").css("height","20px");
-    $("#error_message_password_log").css("text-align","left");
+    $("#error_message_pass_log").html("Please enter your password!");
+    $("#error_message_pass_log").show();
+    $("#login_input_pass").css("border-color","red");
 
-    $("#error_message_password_log").show();
-    $(".inside_box#password_box_log").css("border-color","red");
-    $("#tick_password_log").hide();
     error_password_log = true;
   } else {
-    $("#error_message_password_log").hide();
-    $(".inside_box#password_box_log").css("border-color","white");
-    $("#tick_password_log").show();
+    $("#error_message_pass_log").hide();
+    $("#login_input_pass").css("border-color","green");
     error_password_log = false;
   }
 }
-*/
